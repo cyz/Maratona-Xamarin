f61524c774e4c9f78a39ad2dcc5c26d4898cf739
Letticia Nicoli <letticia.nicoli@lambda3.com.br>
2018-05-29 23:57:37 -0300

    Update demo styles

--- src/Evntr.Core/Views/AboutView.xaml.cs
+++ src/Evntr.Core/Views/AboutView.xaml.cs
@@ -7,29 +7,29 @@ using Xamarin.Forms;
 
 namespace Evntr.Core.Views
 {
-	public partial class AboutView : ContentPage
-	{
-		public AboutView()
-		{
-			InitializeComponent();
-		}
+    public partial class AboutView : ContentPage
+    {
+        public AboutView()
+        {
+            InitializeComponent();
+        }
 
-		protected override async void OnAppearing()
-		{
-			base.OnAppearing();
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
 
-			if (BindingContext is IHandleViewAppearing viewAware)
-			{
-				await viewAware.OnViewAppearingAsync(this);
-			}
-		}
+            if (BindingContext is IHandleViewAppearing viewAware)
+            {
+                await viewAware.OnViewAppearingAsync(this);
+            }
+        }
 
-		protected override async void OnDisappearing()
-		{
-			if (BindingContext is IHandleViewDisappearing viewAware)
-			{
-				await viewAware.OnViewDisappearingAsync(this);
-			}
-		}
-	}
+        protected override async void OnDisappearing()
+        {
+            if (BindingContext is IHandleViewDisappearing viewAware)
+            {
+                await viewAware.OnViewDisappearingAsync(this);
+            }
+        }
+    }
 }
